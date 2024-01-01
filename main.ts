a5c9b4ee256f5f440a268c48ed73ee3a4c35a54d
petervs83 <152507995+petervs83@users.noreply.github.com>
2024-01-01 01:04:56 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,12 @@
+basic.forever(function () {
+    music.playTone(196, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Double))
+    music.playTone(247, music.beat(BeatFraction.Half))
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(294, music.beat(BeatFraction.Double))
+    music.playTone(262, music.beat(BeatFraction.Half))
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Breve))
+})
